0dc4e4d9b234ca875bf3f31646de110843fc723a
Alexander Meek <alexandermeek09@gmail.com>
2018-10-11 13:02:56 +0100

    Create Camera class.

---
+++ include/MyCamera.h
@@ -0,0 +1,17 @@
+#ifndef MYCAMERA_H
+#define MYCAMERA_H
+
+
+class MyCamera
+{
+    public:
+        MyCamera();
+        virtual ~MyCamera();
+
+    protected:
+
+    private:
+        MyVector
+};
+
+#endif // MYCAMERA_H
---
+++ src/MyCamera.cpp
@@ -0,0 +1,11 @@
+#include "MyCamera.h"
+
+MyCamera::MyCamera()
+{
+    //ctor
+}
+
+MyCamera::~MyCamera()
+{
+    //dtor
+}
