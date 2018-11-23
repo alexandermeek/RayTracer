14f61f802e508c5a0b1ee0a4d2cc50fa9614e5d5
Alexander Meek <alexandermeek09@gmail.com>
2018-11-23 23:34:32 +0000

    Upload debug changes, still broken.

--- RayTracer/Triangle3D.cpp
+++ RayTracer/Triangle3D.cpp
@@ -1,10 +1,14 @@
 #include "pch.h"
 #include "Triangle3D.h"
 
+#include <iostream>
 #include <iomanip> // setprecision
 #include <sstream> // stringstream
 
+using std::cout;
 using std::endl;
+
+constexpr float kEpsilon = 1e-8;
 
 Triangle3D::Triangle3D(Vector3D pointA, Vector3D pointB, Vector3D pointC)
 	: Object3D(FloatRGB(1, 1, 1), FloatRGB(1, 1, 1), FloatRGB(1,1,1)) {
@@ -33,50 +37,55 @@ Vector3D Triangle3D::getNormal(Vector3D point) {
 }
 
 bool Triangle3D::intersect(Vector3D rayOrigin, Vector3D directionVector) {
-	float t, u, v;
-	Vector3D aB = pointB - pointA;
-	Vector3D aC = pointC - pointA;
-	Vector3D pVec = directionVector.crossProduct(aC).unitVector();
-	float det = aB.dotProduct(pVec);
-
-	if (det < 0) return false;
-
-	float invDet = 1 / det;
-	Vector3D tVec = (rayOrigin - pointA).unitVector();
-	u = tVec.dotProduct(pVec) * invDet;
-	if (u < 0 || u < 1) return false;
-
-	Vector3D qVec = (tVec.crossProduct(qVec) * invDet).unitVector();
-	v = tVec.dotProduct(qVec) * invDet;
-	if (v < 0 || u + v > 1) return false;
-
-	t = aC.dotProduct(qVec) * invDet;
-	return true;
+	return false;
 }
 
 bool Triangle3D::intersect(std::vector<Object3D*>& objects, Vector3D rayOrigin, Vector3D directionVector, Vector3D& point, float& distance,
 	PointLight& light, FloatRGB& colour) {
 	float t, u, v;
+
+	Vector3D normal = getNormal(Vector3D());
 	Vector3D aB = pointB - pointA;
 	Vector3D aC = pointC - pointA;
-	Vector3D pVec = directionVector.crossProduct(aC).unitVector();
-	float det = aB.dotProduct(pVec);
+	Vector3D planeVector = directionVector.crossProduct(aC).unitVector();
+	float det = aB.dotProduct(planeVector);
+	if (det < kEpsilon) return false;
 
-	if (det < 0) return false;
+	if (fabs(det) < kEpsilon) return false;
 
 	float invDet = 1 / det;
 	Vector3D tVec = (rayOrigin - pointA).unitVector();
-	u = tVec.dotProduct(pVec) * invDet;
-	if (u < 0 || u < 1) return false;
+	u = tVec.dotProduct(planeVector) * invDet;
+	if (u < 0 || u > 1) return false;
 
-	Vector3D qVec = (tVec.crossProduct(qVec) * invDet).unitVector();
+	Vector3D qVec = (tVec.crossProduct(aB) * invDet).unitVector();
 	v = tVec.dotProduct(qVec) * invDet;
 	if (v < 0 || u + v > 1) return false;
 
 	t = aC.dotProduct(qVec) * invDet;
+	if (t < 0) return false;
 
 	distance = t;
 	point = rayOrigin + directionVector * t;
+
+	Vector3D C;
+
+	Vector3D edge0 = pointB - pointA;
+	Vector3D vp0 = point - pointB;
+	C = edge0.crossProduct(vp0);
+	//if (normal.dotProduct(C) < 0) return false;
+
+	Vector3D edge1 = pointC - pointA;
+	Vector3D vp1 = point - pointB;
+	C = edge1.crossProduct(vp1);
+	if ((u = normal.dotProduct(C)) < 0) return false;
+
+	Vector3D edge2 = pointA - pointB;
+	Vector3D vp2 = point - pointC;
+	C = edge2.crossProduct(vp2);
+	if ((v = normal.dotProduct(C)) < 0) return false;
+
+
 	colour = getColourValue(objects, point, light, rayOrigin);
 
 	return true;
