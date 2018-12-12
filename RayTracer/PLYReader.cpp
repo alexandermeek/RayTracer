7999f36bf48f2caf8786917a8f08593fb82da67f
Alexander Meek <alexandermeek09@gmail.com>
2018-12-12 16:59:12 +0000

    Add PLYReader to read triangles from a ply file.

---
+++ RayTracer/PLYReader.cpp
@@ -0,0 +1,114 @@
+#include "pch.h"
+#include "PLYReader.h"
+
+#include <fstream>
+#include <iostream>
+
+PLYReader::PLYReader(std::string filename) {
+	this->filename = filename;
+}
+
+
+PLYReader::~PLYReader() {
+
+}
+
+std::string PLYReader::getFilename() {
+	return filename;
+}
+
+int PLYReader::getNumVertices() {
+	return numVertices;
+}
+
+int PLYReader::getNumFaces() {
+	return numFaces;
+}
+
+bool PLYReader::readPLY(std::vector<Object3D*>& objects, std::vector<Vector3D>& vertices) {
+	std::string line;
+	std::ifstream plyFile(filename);
+	int fileStage = 0;
+
+	if (!plyFile.is_open()) {
+		return false;
+	} else {
+		while (std::getline(plyFile, line)) {
+			switch (fileStage) {
+			case 0 :
+				if (line.substr(0, DESC_PLY.length()) == DESC_PLY) {
+					std::cout << std::endl << "Reading a ply file";
+					fileStage = 1;
+				} 
+				else {
+					return false;
+				}
+				break;
+			case 1 :
+				if (line.substr(0, DESC_VERTICES.length()) == DESC_VERTICES) {
+					numVertices = std::stoi(line.substr(DESC_VERTICES.length(), line.length()));
+				} 
+				else {
+					if (line.substr(0, DESC_FACES.length()) == DESC_FACES) {
+						numFaces = std::stoi(line.substr(DESC_FACES.length(), line.length()));
+					}
+					else {
+						if (line.substr(0, DESC_END_HEADER.length()) == DESC_END_HEADER) {
+							fileStage = 2;
+						}
+					}
+				}
+				break;
+			case 2 :
+				if (line.substr(0, 1) == "3") {
+					fileStage = 3;
+					objects.push_back(new Triangle3D(readTriangle(line, vertices)));
+				}
+				else {
+					vertices.push_back(readPoint(line));
+				}
+				break;
+			case 3 :
+				objects.push_back(new Triangle3D(readTriangle(line, vertices)));
+				break;
+			}
+		}
+		plyFile.close();
+	}
+	
+	return true;
+}
+
+Vector3D PLYReader::readPoint(std::string line) {
+	float v[3];
+	int pos = 0;
+	std::string token;
+
+	for (int i = 0; i < 3; i++) {
+		pos = line.find(DELIMITER);
+		token = line.substr(0, pos);
+		v[i] = std::stof(token);
+		line.erase(0, pos + DELIMITER.length());
+	}
+
+	Vector3D vertex(v[0], v[1], v[2]);
+	return vertex;
+}
+
+Triangle3D PLYReader::readTriangle(std::string line, std::vector<Vector3D>& vertices) {
+	int v[3];
+	int pos = 0;
+	std::string token;
+
+	line.erase(0, 2);
+
+	for (int i = 0; i < 3; i++) {
+		pos = line.find(DELIMITER);
+		token = line.substr(0, pos);
+		v[i] = std::stoi(token);
+		line.erase(0, pos + DELIMITER.length());
+	}
+
+	Triangle3D tri(vertices[v[0]], vertices[v[1]], vertices[v[2]]);
+	return tri;
+}
---
+++ RayTracer/PLYReader.h
@@ -0,0 +1,26 @@
+#pragma once
+class PLYReader {
+	public:
+		PLYReader(std::string filename);
+		~PLYReader();
+
+		std::string getFilename();
+		int getNumVertices();
+		int getNumFaces();
+		bool readPLY(std::vector<Object3D*>& objects, std::vector<Vector3D>& vertices);
+	private:
+		std::string filename;
+		int numVertices = 0;
+		int numFaces = 0;
+
+		const std::string DELIMITER = " ";
+		const std::string DESC_PLY = "ply";
+		const std::string DESC_VERTICES = "element vertex";
+		const std::string DESC_FACES = "element face";
+		const std::string DESC_END_HEADER = "end_header";
+
+		Vector3D readPoint(std::string line);
+		Triangle3D readTriangle(std::string line, std::vector<Vector3D>& vertices);
+
+};
+
